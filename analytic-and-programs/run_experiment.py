0be80c7754d23c6e74ca4f4a3cfc288f5757fe2a
RohanD1369 <84051203+RohanD1369@users.noreply.github.com>
2024-08-07 20:07:25 -0400

    Update run_experiment.py

--- analytic-and-programs/run_experiment.py
+++ analytic-and-programs/run_experiment.py
@@ -2,15 +2,17 @@ import json
 import argparse
 from experiment import ExperimentCollection, Experiment
 
-experiments_folder = "./experiments/"
+# Assuming the JSON file is in the root directory of Colab
+json_file_path = "/content/parameters.json"
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--collection_name', type=str, default='basic_collection')
     args = parser.parse_args()
 
     experiment_collection = ExperimentCollection(args.collection_name)
-    with open(content_folder + args.parameters + '.json', 'r') as f:
+    
+    with open(json_file_path, 'r') as f:
         parameters = json.load(f)
         for experiment_params in parameters['collection']:
             experiment_collection.push(Experiment(**experiment_params))
